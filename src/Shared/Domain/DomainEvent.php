18075213b649a0d8cd5db210857085c1503925ab
Yahir <yahir.eduardo.bravo@gmail.com>
2025-09-07 20:52:15 -0500

    feat: add contract for domain events

---
+++ src/Shared/Domain/DomainEvent.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Domain;
+
+use DateTimeImmutable;
+
+interface DomainEvent
+{
+    public function aggregateId(): string;
+    public function ocurredOn(): DateTimeImmutable;
+    public static function eventName(): string;
+}
